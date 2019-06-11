687ecc41e8884af8b608e04eebdead572e90ba54
David Luecke <daff@neyeon.com>
2019-06-11 16:37:55 -0700

    Finalizing slides

--- snippets/message-middleware.js
+++ snippets/message-middleware.js
@@ -7,7 +7,10 @@ class MessageService {
     addCreatedAt,
     sendEmailNotification
   ])
-  create(data, params) {},
+  create(data, params) {
+    // Insert data e.g. into MongoDB
+    return mongodb.collection('messages').insertOne(data);
+  }
 
   @middleware([
     checkPermission
--- snippets/message-service.js
+++ snippets/message-service.js
@@ -1,15 +1,15 @@
 class MessageService {
 
   // Find a list of resources
-  find(params) {},
+  find(params) {}
   // Get a single resource by its id
-  get(id, params) {},
+  get(id, params) {}
   // Create a new resource
-  create(data, params) {},
+  create(data, params) {}
   // Replace an existing resource by its id with data
-  update(id, data, params) {},
+  update(id, data, params) {}
   // Merge new data into a resource
-  patch(id, data, params) {},
+  patch(id, data, params) {}
   // Remove a resource by its id
   remove(id, params) {}
 
